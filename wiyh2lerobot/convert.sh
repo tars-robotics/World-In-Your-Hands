9caf922a7283a3f9ecfe610590238f3a2eebf38b
BlackypZheng <zhengyupeng@tars-ai.com>
2025-12-26 13:52:08 +0800

    update process h5 file

--- wiyh2lerobot/convert.sh
+++ wiyh2lerobot/convert.sh
@@ -3,7 +3,7 @@ export HF_DATASETS_DISABLE_PROGRESS_BARS=TRUE
 export HDF5_USE_FILE_LOCKING=FALSE
 
 python wiyh_h5.py \
-    --src-paths /mnt/data/data/zyp/wiyh/wiyh-sdk/data/tmp \
+    --src-paths ./data/tmp \
     --output-path wiyh_lerobot \
     --executor local \
     --tasks-per-job 3 \
